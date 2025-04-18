25b1add0f2508cebe5805ccb3270695db443b041
netdcy <netdcy@163.com>
2025-04-18 17:09:07 +0800

    flowvision 1.6.2

--- Casks/flowvision.rb
+++ Casks/flowvision.rb
@@ -1,11 +1,12 @@
 cask "flowvision" do
-  version "1.6.1"
-  sha256 :no_check
+  version "1.6.2"
+  sha256 "c74e07d21927371a7ebcc5d7785416c1e9c3efe3d5c9b7e1fc99ba7b6631ce8a"
 
-  url "https://github.com/netdcy/FlowVision/releases/download/#{version}/FlowVision.#{version}.zip"
+  url "https://github.com/netdcy/FlowVision/releases/download/#{version}/FlowVision.#{version}.zip",
+      verified: "github.com/netdcy/FlowVision/"
   name "FlowVision"
-  desc "Waterfall-style Image Viewer for macOS"
-  homepage "https://github.com/netdcy/FlowVision"
+  desc "Waterfall-style image viewer"
+  homepage "https://flowvision.app/"
 
   livecheck do
     url :url
